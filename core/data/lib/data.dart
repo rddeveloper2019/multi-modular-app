b0282b5ec2b0b9dffff6962c301b0b5c9f203f30
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2026-04-14 00:04:17 +0300

    wrong deps share

--- core/data/lib/data.dart
+++ core/data/lib/data.dart
@@ -1,4 +1,8 @@
+import 'package:dio/dio.dart';
+export 'package:dio/dio.dart';
+
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+  Dio dio = Dio();
 }
--- features/auth/login/lib/login.dart
+++ features/auth/login/lib/login.dart
@@ -1,5 +1,7 @@
-/// A Calculator.
+import 'package:data/data.dart';
+
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+  Dio dio = Dio();
 }
