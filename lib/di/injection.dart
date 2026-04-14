d99ee0728f2443512d88b96992270b8000164594
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2026-04-14 18:13:22 +0300

    injectable first generate

---
+++ lib/di/injection.dart
@@ -0,0 +1,13 @@
+import 'package:injectable/injectable.dart';
+import 'package:multimodular_app/di/injection.config.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+@InjectableInit(
+  initializerName: 'init',
+  preferRelativeImports: true,
+  asExtension: true,
+)
+void configureDependencies(String? environment) =>
+    getIt.init(environment: environment);
