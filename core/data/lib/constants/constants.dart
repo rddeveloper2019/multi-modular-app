0f870e70d9ae40492226f5222a336727b91110c4
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2026-04-14 21:59:50 +0300

    add dio factory

---
+++ core/data/lib/constants/constants.dart
@@ -0,0 +1,8 @@
+const String applicationJson = 'application/json';
+const String contentType = 'content-type';
+const String accept = 'accept';
+const String authorization = 'authorization';
+
+//custom headers
+const String defaultLanguage = 'default-language';
+const String clientId = 'client-id';
