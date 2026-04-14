53fb075c3cf6a8eb735bdfbe6338f3afca93bb12
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2026-04-14 23:54:03 +0300

    add mapper

---
+++ features/auth/login/lib/data/mapper/login_mapper.dart
@@ -0,0 +1,8 @@
+import 'package:login/data/response/login_response.dart';
+import 'package:login/domain/model/login_model.dart';
+
+extension LoginResponseMapper on LoginResponse {
+  LoginModel toDomain() {
+    return LoginModel(name, phone, age);
+  }
+}
